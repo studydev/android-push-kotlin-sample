af3d75b95c41b5969dd4ee81bcd59bae87f5f991
bjaehyun <bjaehyun@amazon.com>
2020-09-23 15:05:53 +0900

    fix push listener service

--- app/src/main/java/kr/jhb/androidpushktsample/PushListenerService.kt
+++ app/src/main/java/kr/jhb/androidpushktsample/PushListenerService.kt
@@ -1,7 +1,12 @@
 package kr.jhb.androidpushktsample
 
+import android.content.Intent
+import android.os.Bundle
 import android.util.Log
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.amazonaws.mobileconnectors.pinpoint.targeting.notification.NotificationClient
+import com.amazonaws.mobileconnectors.pinpoint.targeting.notification.NotificationClient.INTENT_SNS_NOTIFICATION_DATA
+import com.amazonaws.mobileconnectors.pinpoint.targeting.notification.NotificationClient.INTENT_SNS_NOTIFICATION_FROM
 import com.amazonaws.mobileconnectors.pinpoint.targeting.notification.NotificationDetails
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
@@ -13,8 +18,8 @@ class PushListenerService : FirebaseMessagingService() {
     override fun onNewToken(token: String?) {
         super.onNewToken(token)
 
+        Log.d(TAG,"Registring push notifications token: $token")
         MainActivity.pinpointManager?.notificationClient?.registerDeviceToken(token)
-        Log.d(TAG,"$token registered")
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
@@ -29,7 +34,27 @@ class PushListenerService : FirebaseMessagingService() {
             .build()
 
         val pushResult = notificationClient?.handleNotificationReceived(notificationDetails)
+    }
 
+    private fun broadcast(from: String?, dataMap: HashMap<String, String>) {
+        val intent = Intent(ACTION_PUSH_NOTIFICATION)
+        intent.putExtra(INTENT_SNS_NOTIFICATION_FROM, from)
+        intent.putExtra(INTENT_SNS_NOTIFICATION_DATA, dataMap)
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+    }
 
+    companion object {
+        // Intent action used in local broadcast
+        const val ACTION_PUSH_NOTIFICATION = "push-notification"
+
+        /**
+         * Helper method to extract push message from bundle.
+         *
+         * @param data bundle
+         * @return message string from push notification
+         */
+        fun getMessage(data : Bundle): String {
+            return data.get("data").toString()
+        }
     }
 }
