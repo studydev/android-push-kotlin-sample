b4823226badd050431e7bb462ef95d5910b46ebd
bjaehyun <bjaehyun@amazon.com>
2020-09-23 13:58:25 +0900

    add pinpoint manager in mainactivity

--- app/src/main/java/kr/jhb/androidpushktsample/MainActivity.kt
+++ app/src/main/java/kr/jhb/androidpushktsample/MainActivity.kt
@@ -1,38 +1,87 @@
 package kr.jhb.androidpushktsample
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import com.amazonaws.mobile.client.AWSMobileClient
+import com.amazonaws.mobile.client.Callback
+import com.amazonaws.mobile.client.UserStateDetails
+import com.amazonaws.mobile.config.AWSConfiguration
+import com.amazonaws.mobileconnectors.pinpoint.PinpointConfiguration
+import com.amazonaws.mobileconnectors.pinpoint.PinpointManager
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.iid.InstanceIdResult
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
+    val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        getPinpointManager(applicationContext)
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        FirebaseInstanceId.getInstance().instanceId
+                .addOnCompleteListener(object : OnCompleteListener<InstanceIdResult>{
+                    override fun onComplete(task: Task<InstanceIdResult>) {
+                        if (!task.isSuccessful) {
+                            Log.w(TAG, "getInstanceId failed", task.exception)
+                            return
+                        }
+                        val token = task.getResult().token
+                        Log.d(TAG, "Registering push notification token: $token")
+                        pinpointManager?.notificationClient?.registerDeviceToken(token)
+                    }
+
+                })
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    fun getPinpointManager(applicationContext: Context): PinpointManager {
+        if (pinpointManager != null)
+            return pinpointManager as PinpointManager
+
+        val awsConfig = AWSConfiguration(applicationContext)
+        val awsClient = AWSMobileClient.getInstance()
+
+        awsClient.initialize(applicationContext, awsConfig, object : Callback<UserStateDetails>{
+            override fun onResult(result: UserStateDetails?) {
+                Log.i(TAG, result?.userState.toString())
+            }
+
+            override fun onError(e: Exception?) {
+                Log.e(TAG, "Initialization error.", e)
+            }
+
+        })
+
+        val pinpointConfig = PinpointConfiguration(
+            applicationContext,
+            AWSMobileClient.getInstance(),
+            awsConfig
+        )
+
+        pinpointManager = PinpointManager(pinpointConfig)
+        return pinpointManager as PinpointManager
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+
+    companion object {
+        var pinpointManager: PinpointManager? = null
     }
+
 }
