b557bd19ffde94f9464d67dd1de8bc78f9fc2c26
bjaehyun <bjaehyun@amazon.com>
2020-09-23 14:31:56 +0900

    add push service

---
+++ app/src/main/java/kr/jhb/androidpushktsample/PushListenerService.kt
@@ -0,0 +1,35 @@
+package kr.jhb.androidpushktsample
+
+import android.util.Log
+import com.amazonaws.mobileconnectors.pinpoint.targeting.notification.NotificationClient
+import com.amazonaws.mobileconnectors.pinpoint.targeting.notification.NotificationDetails
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class PushListenerService : FirebaseMessagingService() {
+
+    private val TAG = "PushListenerService"
+
+    override fun onNewToken(token: String?) {
+        super.onNewToken(token)
+
+        MainActivity.pinpointManager?.notificationClient?.registerDeviceToken(token)
+        Log.d(TAG,"$token registered")
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        Log.d(TAG, "Message : $remoteMessage.data")
+
+        val notificationClient = MainActivity.pinpointManager?.notificationClient
+        val notificationDetails = NotificationDetails.builder()
+            .from(remoteMessage.from)
+            .mapData(remoteMessage.data)
+            .intentAction(NotificationClient.FCM_INTENT_ACTION)
+            .build()
+
+        val pushResult = notificationClient?.handleNotificationReceived(notificationDetails)
+
+
+    }
+}
